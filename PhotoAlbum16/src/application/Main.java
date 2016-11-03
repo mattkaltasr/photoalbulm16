2aaec72faaad88858ffc4df516a9d8f5efa4ac9f
Yigit Gungor <yigitgungor@gmail.com>
2016-11-03 19:47:47 -0400

    Login Page UI (FXML)

--- PhotoAlbum16/src/application/Main.java
+++ PhotoAlbum16/src/application/Main.java
@@ -1,20 +1,26 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/views/LoginPage.fxml"));
+			AnchorPane root = (AnchorPane)loader.load();
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
-			primaryStage.show();
+			primaryStage.setTitle("Photo Album");
+			primaryStage.show();			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
