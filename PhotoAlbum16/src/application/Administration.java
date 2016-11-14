5cd7792c46f2dcb9cd89d99f6d93963f7838879c
Yigit Gungor <yigitgungor@gmail.com>
2016-11-14 16:33:43 -0500

    ,

---
+++ PhotoAlbum16/src/application/Administration.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.ArrayList;
+import application.Role;
+
+public class Administration extends User {
+	
+	private final static Role Admin=Role.admin;
+	private ArrayList<User> materUserList;
+	
+	
+	
+	
+	
+	public Administration(String user,String pass){
+		super(user,pass,Admin);
+		
+		materUserList=new ArrayList<User>();
+	}
+
+	
+	
+	
+	
+	public ArrayList<User> getList(){
+		
+		return materUserList;
+	}
+}
