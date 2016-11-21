5faf3367f494dc5ed322d775f43db75bfc9fffbf
mattkaltasr <mkalitasr@gmail.com>
2016-11-21 12:02:24 -0500

    fixed login issue with list now logs in with new add

--- PhotoAlbum16/src/application/Main.java
+++ PhotoAlbum16/src/application/Main.java
@@ -30,6 +30,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		try {
 			getUserList();
+			UserList=admin.getList();
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("/views/LoginPage.fxml"));
 			AnchorPane root = (AnchorPane)loader.load();
