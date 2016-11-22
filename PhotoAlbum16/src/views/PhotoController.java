dae537f4c730d331b8df7a1366bfb21ebfe76ee9
Yigit Gungor <yigitgungor@gmail.com>
2016-11-22 08:16:45 -0500

    -UML Added (ucls format). -Add/Remove Tag buttons/methods added -UI changes

--- PhotoAlbum16/src/views/PhotoController.java
+++ PhotoAlbum16/src/views/PhotoController.java
@@ -2,6 +2,7 @@ package views;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 
 import application.Main;
 import application.Photo;
@@ -10,10 +11,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
-
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -29,18 +32,19 @@ public class PhotoController {
 	@FXML
 	private Button deleteButton;
 	
+	Photo p = null;
+	
 	public void start(Stage mainStage) throws Exception {      
 		initialize();
 	}
 	
 	@FXML
 	public void initialize(){
-		Photo p = AlbumPhotosPageController.currentphoto;
+		p = AlbumPhotosPageController.currentphoto;
 		if(p != null){
 		photoImgView.setImage(p.getImage());
 		nameLabel.setText(p.getName());
-		//GET TAGS FROM HASHMAP
-		tagsLabel.setText("");
+		tagsLabel.setText(p.getTagDisplay());
 		}
 	}
 	
@@ -68,7 +72,62 @@ public class PhotoController {
 		backButton_Clicked(event);
 	}
 	
+	@FXML
+	public void addTagButton_Clicked(ActionEvent event) throws IOException {
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle("New Tag");
+		dialog.setHeaderText("Add a new tag");
+		dialog.setContentText("Tag: ");
+		Optional<String> result = dialog.showAndWait();
+
+		String tag = null;
+		if(result.isPresent()){
+			tag = result.get();
+		}
+		
+		if(tag != null && !p.getTagDisplay().contains("tag")){
+			// ADD THE TAG TO THE PHOTO P
+			// UPDATE THE FILE/SERIALIZED OBJECT TOO
+			// ALSO UPDATE "AlbumPhotosPageController.currentphoto"
+			initialize();
+		}
+		else{
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error Dialog");
+			alert.setHeaderText("Something is wrong.");
+			alert.setContentText("This tag either exists or we can't find the photo you want to edit.");
+			alert.showAndWait();
+		}
+	}
 	
-	
+	@FXML
+	public void removeTagButton_Clicked(ActionEvent event) throws IOException {
+		
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle("Remove Tag");
+		dialog.setHeaderText("Remove a tag");
+		dialog.setContentText("Which tag do you want to remove?: ");
+		Optional<String> result = dialog.showAndWait();
+
+		String tag = null;
+		if(result.isPresent()){
+			tag = result.get();
+		}
+		
+		if(tag != null && p.getTagDisplay().contains("tag")){
+			// REMOVE THE TAG TO THE PHOTO P
+			// UPDATE THE FILE/SERIALIZED OBJECT TOO
+			// ALSO UPDATE "AlbumPhotosPageController.currentphoto"
+			initialize();
+		}
+		else{
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error Dialog");
+			alert.setHeaderText("Something is wrong.");
+			alert.setContentText("This tag either doesn't exist or we can't find the photo you want to edit.");
+			alert.showAndWait();
+		}
+		
+	}
 	
 }
